9d409e10e755503f436de492c8a1bed70434e09f
itush <itusharbiswas@gmail.com>
2022-11-27 22:38:13 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './index.scss'
+import Content from './Content'
+import Footer from './Footer'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <Content />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer className='"attribution"'>
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+            Coded by <a href="#">Tushar Biswas</a>.
+        </footer>
+    )
+}
+
+export default Footer;
